cae50bd9d956cbee44ff96de2fcde09f5f6fd338
MyaMya0709 <chlehdgh96@gmail.com>
2025-03-27 15:26:58 +0900

    wip UI 추가 중
    - 아이콘 추가

--- Assets/02.Scripts/Weapons/WeaponData.cs
+++ Assets/02.Scripts/Weapons/WeaponData.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewItem", menuName = "Item/Weapon")]
 public class WeaponData : ScriptableObject
 {
+    //무기 정보
     [Header("Info")]
-    public int WeaponLevel;
-    public string WeaponName;
-    public int AttackValum;
-    public int CriticalProbability;
+    public int weaponLevel;               //무기 레벨
+    public string weaponName;             //무기 이름
+    public int attackValum;               //무기 공격력
+    [Range(0f,100f)]                      //치명타 확률 범위
+    public float criticalChance;          //치명타 확률 %단위
+    [Range(0f,600f)]                        //치명타 배율 범위
+    public float criticalMultiplier = 100f; //치명타 배율 기본 100%
 
-    [Header("Upgrade")]
-    public int ATK_ValumUp;
-    public int CRI_ProbabilityUp;
+    //무기 업그레이드 정보
+    [Header("UpgradeInfo")]
+    public int upgradeNumber;       //강화 횟수
+    public int attackValum_Up;      //공격력 강화 수치
+    public float criticalChance_Up; //치명타 확률 강화 수치
+    public float criticalPower_Up;  //치명타 배율 강화 수치
+    public int upgradeCost;         //업그레이드 비용
+    public int upgradeCost_UP;      //업그레이드 비용 상승 수치
 
 }
